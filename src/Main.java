a743bad9569d80920af58cdf7a33ea3848e478fb
Rikiya Ishikawa <flower.night.flower@gmail.com>
2026-03-24 18:52:09 +0900

    リファクタリング: Mainからキー入力処理を切り離し、InputHandlerとして分離

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -14,10 +13,11 @@ import javax.swing.JPanel;
 *RPGゲームである、十字キーでキャラクターを動かす仕組みを作成してください
 */
 
-public class Main extends JPanel implements KeyListener{
+public class Main extends JPanel{
 	
 	private GameMap gameMap = new GameMap(); //GameMap.javaを追加(アップデート1)
 	private boolean isGameCleared = false;
+	private InputHandler inputHandler;
 	
 	//キャラの座標
 	private int x = 50;
@@ -31,7 +31,8 @@ public class Main extends JPanel implements KeyListener{
 	  frame.setSize(400, 400);
 	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	  frame.add(this);
-	  frame.addKeyListener(this);
+	  inputHandler = new InputHandler();
+	  frame.addKeyListener(inputHandler);
 	  frame.setVisible(true);
 	  try {
 		  Path imagePath = Path.of("ref/pipo-charachip029a.png");
@@ -39,7 +40,7 @@ public class Main extends JPanel implements KeyListener{
 		  this.playerImage = fullSheet.getSubimage(0, 0, 32, 32); 
 	  } catch (IOException e) {
 		  System.err.println("画像が見つかりません。四角で代用します: " + e.getMessage());
-	  }
+	  }	  
 	}
 	
 	// 画面を描くメソッド
@@ -70,41 +71,7 @@ public class Main extends JPanel implements KeyListener{
 	        return;  // ← ここでメソッド終了
 	    }
 
-		if(key == KeyEvent.VK_LEFT) {
-			int nextX = x - speed;
-			boolean topLeft = (gameMap.isWall(nextX, y) == false);
-	        boolean bottomLeft = (gameMap.isWall(nextX, y + CHARSIZE - 1) == false);
-			if (topLeft && bottomLeft) {
-				x = nextX;
-			}
-		}
-			
-		if(key == KeyEvent.VK_RIGHT) {
-			int nextX = x + speed;
-			boolean topRight = (gameMap.isWall(nextX + CHARSIZE - 1, y) == false);
-	        boolean bottomRight = (gameMap.isWall(nextX + CHARSIZE - 1, y + CHARSIZE - 1) == false);
-			if (topRight && bottomRight) {
-				x = nextX;
-			}
-		}
-		
-		if(key == KeyEvent.VK_UP) {
-			int nextY = y - speed;
-			boolean topLeft = (gameMap.isWall(x, nextY) == false);
-	        boolean topRight = (gameMap.isWall(x + CHARSIZE - 1, nextY) == false);
-			if (topLeft && topRight) {
-				y = nextY;
-			}
-		}
-		
-		if(key == KeyEvent.VK_DOWN) {
-			int nextY = y + speed;
-			boolean bottomLeft = (gameMap.isWall(x, nextY + CHARSIZE - 1) == false);
-	        boolean bottomRight = (gameMap.isWall(x + CHARSIZE - 1, nextY + CHARSIZE - 1) == false);
-			if (bottomLeft && bottomRight) {
-				y = nextY;
-			}
-		}
+	    playerMover.move(this, gameMap, inputHandler);
 		
 		// ★ ゴール判定 ★
 		if (gameMap.isGoal(x, y)) {
