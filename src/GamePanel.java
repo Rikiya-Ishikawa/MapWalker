4e5fe00bba04bd3f0567b0e970bc2e8ef4e9650e
Rikiya Ishikawa <flower.night.flower@gmail.com>
2026-03-27 18:38:19 +0900

    GameMapの利用箇所をMainからGamePanelに移動(GameMap本体は無変更)

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,11 +1,22 @@
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
 
     private InputHandler inputHandler;
+    private GameMap gameMap;
+
 
     public GamePanel() {
         inputHandler = new InputHandler();
         this.addKeyListener(inputHandler);
+        gameMap = new GameMap();
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        gameMap.draw(g);
     }
 }
